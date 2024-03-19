fce58f84c2fa4ab0aea8d1432a12675faa743d8f
Vanduyn39 <152328949+Vanduyn39@users.noreply.github.com>
2024-03-19 11:01:35 +0700

    Add files via upload

---
+++ Vòng 1/DEM NGUOC/DEM NGUOC/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace DEM_NGUOC
+{
+    partial class Main_DN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Panel4 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbl_dvi = new System.Windows.Forms.Label();
+            this.guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbl_chuc = new System.Windows.Forms.Label();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbl_tram = new System.Windows.Forms.Label();
+            this.Panel_1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbl_nghin1 = new System.Windows.Forms.Label();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.btn_chơi = new Guna.UI2.WinForms.Guna2Button();
+            this.btn_kiemtra = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
+            this.guna2Panel4.SuspendLayout();
+            this.guna2Panel3.SuspendLayout();
+            this.guna2Panel2.SuspendLayout();
+            this.Panel_1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(302, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐẾM NGƯỢC";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderColor = System.Drawing.Color.Black;
+            this.guna2Panel1.BorderThickness = 5;
+            this.guna2Panel1.Controls.Add(this.guna2Panel4);
+            this.guna2Panel1.Controls.Add(this.guna2Panel3);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Controls.Add(this.Panel_1);
+            this.guna2Panel1.Location = new System.Drawing.Point(276, 174);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(484, 339);
+            this.guna2Panel1.TabIndex = 1;
+            // 
+            // guna2Panel4
+            // 
+            this.guna2Panel4.BackColor = System.Drawing.Color.Black;
+            this.guna2Panel4.BorderColor = System.Drawing.Color.Maroon;
+            this.guna2Panel4.BorderThickness = 5;
+            this.guna2Panel4.Controls.Add(this.lbl_dvi);
+            this.guna2Panel4.Location = new System.Drawing.Point(375, 3);
+            this.guna2Panel4.Name = "guna2Panel4";
+            this.guna2Panel4.Size = new System.Drawing.Size(107, 333);
+            this.guna2Panel4.TabIndex = 3;
+            // 
+            // lbl_dvi
+            // 
+            this.lbl_dvi.AutoSize = true;
+            this.lbl_dvi.BackColor = System.Drawing.Color.White;
+            this.lbl_dvi.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dvi.Location = new System.Drawing.Point(9, 104);
+            this.lbl_dvi.Name = "lbl_dvi";
+            this.lbl_dvi.Size = new System.Drawing.Size(95, 109);
+            this.lbl_dvi.TabIndex = 2;
+            this.lbl_dvi.Text = "0";
+            // 
+            // guna2Panel3
+            // 
+            this.guna2Panel3.BackColor = System.Drawing.Color.Black;
+            this.guna2Panel3.BorderColor = System.Drawing.Color.Maroon;
+            this.guna2Panel3.BorderThickness = 5;
+            this.guna2Panel3.Controls.Add(this.lbl_chuc);
+            this.guna2Panel3.Location = new System.Drawing.Point(250, 3);
+            this.guna2Panel3.Name = "guna2Panel3";
+            this.guna2Panel3.Size = new System.Drawing.Size(107, 333);
+            this.guna2Panel3.TabIndex = 2;
+            // 
+            // lbl_chuc
+            // 
+            this.lbl_chuc.AutoSize = true;
+            this.lbl_chuc.BackColor = System.Drawing.Color.White;
+            this.lbl_chuc.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_chuc.Location = new System.Drawing.Point(9, 104);
+            this.lbl_chuc.Name = "lbl_chuc";
+            this.lbl_chuc.Size = new System.Drawing.Size(95, 109);
+            this.lbl_chuc.TabIndex = 2;
+            this.lbl_chuc.Text = "0";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.Color.Black;
+            this.guna2Panel2.BorderColor = System.Drawing.Color.Maroon;
+            this.guna2Panel2.BorderThickness = 5;
+            this.guna2Panel2.Controls.Add(this.lbl_tram);
+            this.guna2Panel2.Location = new System.Drawing.Point(126, 3);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.Size = new System.Drawing.Size(107, 333);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // lbl_tram
+            // 
+            this.lbl_tram.AutoSize = true;
+            this.lbl_tram.BackColor = System.Drawing.Color.White;
+            this.lbl_tram.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_tram.Location = new System.Drawing.Point(9, 104);
+            this.lbl_tram.Name = "lbl_tram";
+            this.lbl_tram.Size = new System.Drawing.Size(95, 109);
+            this.lbl_tram.TabIndex = 2;
+            this.lbl_tram.Text = "0";
+            // 
+            // Panel_1
+            // 
+            this.Panel_1.BackColor = System.Drawing.Color.Black;
+            this.Panel_1.BorderColor = System.Drawing.Color.Maroon;
+            this.Panel_1.BorderThickness = 5;
+            this.Panel_1.Controls.Add(this.lbl_nghin1);
+            this.Panel_1.Location = new System.Drawing.Point(3, 3);
+            this.Panel_1.Name = "Panel_1";
+            this.Panel_1.Size = new System.Drawing.Size(107, 333);
+            this.Panel_1.TabIndex = 0;
+            this.Panel_1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel_1_Paint);
+            // 
+            // lbl_nghin1
+            // 
+            this.lbl_nghin1.AutoSize = true;
+            this.lbl_nghin1.BackColor = System.Drawing.Color.White;
+            this.lbl_nghin1.Font = new System.Drawing.Font("Times New Roman", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_nghin1.Location = new System.Drawing.Point(9, 104);
+            this.lbl_nghin1.Name = "lbl_nghin1";
+            this.lbl_nghin1.Size = new System.Drawing.Size(95, 109);
+            this.lbl_nghin1.TabIndex = 3;
+            this.lbl_nghin1.Text = "0";
+            this.lbl_nghin1.Click += new System.EventHandler(this.lbl_nghin1_Click);
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Font = new System.Drawing.Font("Times New Roman", 25.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_timer.Location = new System.Drawing.Point(111, 190);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(103, 78);
+            this.lbl_timer.TabIndex = 2;
+            this.lbl_timer.Text = "60";
+            // 
+            // btn_chơi
+            // 
+            this.btn_chơi.BackColor = System.Drawing.Color.Transparent;
+            this.btn_chơi.BorderColor = System.Drawing.Color.Maroon;
+            this.btn_chơi.BorderRadius = 3;
+            this.btn_chơi.BorderThickness = 5;
+            this.btn_chơi.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_chơi.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_chơi.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_chơi.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_chơi.FillColor = System.Drawing.Color.Black;
+            this.btn_chơi.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_chơi.ForeColor = System.Drawing.Color.White;
+            this.btn_chơi.Location = new System.Drawing.Point(82, 326);
+            this.btn_chơi.Name = "btn_chơi";
+            this.btn_chơi.Size = new System.Drawing.Size(164, 64);
+            this.btn_chơi.TabIndex = 3;
+            this.btn_chơi.Text = "CHƠI";
+            // 
+            // btn_kiemtra
+            // 
+            this.btn_kiemtra.BackColor = System.Drawing.Color.Transparent;
+            this.btn_kiemtra.BorderColor = System.Drawing.Color.Gold;
+            this.btn_kiemtra.BorderRadius = 3;
+            this.btn_kiemtra.BorderThickness = 5;
+            this.btn_kiemtra.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_kiemtra.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_kiemtra.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_kiemtra.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_kiemtra.FillColor = System.Drawing.Color.Black;
+            this.btn_kiemtra.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_kiemtra.ForeColor = System.Drawing.Color.White;
+            this.btn_kiemtra.Location = new System.Drawing.Point(790, 281);
+            this.btn_kiemtra.Name = "btn_kiemtra";
+            this.btn_kiemtra.Size = new System.Drawing.Size(164, 109);
+            this.btn_kiemtra.TabIndex = 4;
+            this.btn_kiemtra.Text = "KIỂM TRA";
+            // 
+            // Main_DN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::DEM_NGUOC.Properties.Resources._432348082_380713308086124_1446086889351010626_n;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.btn_kiemtra);
+            this.Controls.Add(this.btn_chơi);
+            this.Controls.Add(this.lbl_timer);
+            this.Controls.Add(this.guna2Panel1);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main_DN";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel4.ResumeLayout(false);
+            this.guna2Panel4.PerformLayout();
+            this.guna2Panel3.ResumeLayout(false);
+            this.guna2Panel3.PerformLayout();
+            this.guna2Panel2.ResumeLayout(false);
+            this.guna2Panel2.PerformLayout();
+            this.Panel_1.ResumeLayout(false);
+            this.Panel_1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel Panel_1;
+        private System.Windows.Forms.Label lbl_nghin;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel4;
+        private System.Windows.Forms.Label lbl_dvi;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private System.Windows.Forms.Label lbl_chuc;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Label lbl_tram;
+        private System.Windows.Forms.Label lbl_nghin1;
+        private System.Windows.Forms.Label lbl_timer;
+        private Guna.UI2.WinForms.Guna2Button btn_chơi;
+        private Guna.UI2.WinForms.Guna2Button btn_kiemtra;
+    }
+}
+
