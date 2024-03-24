3af79cccd8c4045558de122e86ba6b6085547b5e
Vanduyn39 <152328949+Vanduyn39@users.noreply.github.com>
2024-03-24 19:43:51 +0700

    Add files via upload

---
+++ DEM NGUOC/DEM NGUOC/Giaithuong_DN.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace DEM_NGUOC
+{
+    partial class Giaithuong_DN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_tieptuc = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.cua_1 = new System.Windows.Forms.PictureBox();
+            this.cua_2 = new System.Windows.Forms.PictureBox();
+            this.bt_batdau = new Guna.UI2.WinForms.Guna2Button();
+            this.panel_cm = new Guna.UI2.WinForms.Guna2Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cua_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cua_2)).BeginInit();
+            this.panel_cm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(78, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(863, 49);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "GIẢI THƯỞNG VÀ SẢN PHẨM ĐOÁN GIÁ";
+            // 
+            // btn_tieptuc
+            // 
+            this.btn_tieptuc.BackColor = System.Drawing.Color.Transparent;
+            this.btn_tieptuc.BorderColor = System.Drawing.Color.Gold;
+            this.btn_tieptuc.BorderRadius = 3;
+            this.btn_tieptuc.BorderThickness = 5;
+            this.btn_tieptuc.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_tieptuc.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_tieptuc.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_tieptuc.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_tieptuc.FillColor = System.Drawing.Color.Black;
+            this.btn_tieptuc.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_tieptuc.ForeColor = System.Drawing.Color.White;
+            this.btn_tieptuc.Location = new System.Drawing.Point(781, 500);
+            this.btn_tieptuc.Name = "btn_tieptuc";
+            this.btn_tieptuc.Size = new System.Drawing.Size(179, 66);
+            this.btn_tieptuc.TabIndex = 3;
+            this.btn_tieptuc.Text = "Tiếp tục";
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox2.BackgroundImage = global::DEM_NGUOC.Properties.Resources.snapedit_1710818610279;
+            this.guna2PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.guna2PictureBox2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(255, 66);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(474, 500);
+            this.guna2PictureBox2.TabIndex = 1;
+            this.guna2PictureBox2.TabStop = false;
+            // 
+            // cua_1
+            // 
+            this.cua_1.BackgroundImage = global::DEM_NGUOC.Properties.Resources._432453464_793900792591927_3311968906852197221_n;
+            this.cua_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cua_1.Location = new System.Drawing.Point(0, 0);
+            this.cua_1.Name = "cua_1";
+            this.cua_1.Size = new System.Drawing.Size(500, 600);
+            this.cua_1.TabIndex = 4;
+            this.cua_1.TabStop = false;
+            // 
+            // cua_2
+            // 
+            this.cua_2.BackgroundImage = global::DEM_NGUOC.Properties.Resources._423903942_726671656268022_8227592408462440658_n;
+            this.cua_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cua_2.Location = new System.Drawing.Point(500, 0);
+            this.cua_2.Name = "cua_2";
+            this.cua_2.Size = new System.Drawing.Size(500, 600);
+            this.cua_2.TabIndex = 5;
+            this.cua_2.TabStop = false;
+            // 
+            // bt_batdau
+            // 
+            this.bt_batdau.BackColor = System.Drawing.Color.Transparent;
+            this.bt_batdau.BorderColor = System.Drawing.Color.Gold;
+            this.bt_batdau.BorderRadius = 2;
+            this.bt_batdau.BorderThickness = 4;
+            this.bt_batdau.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.bt_batdau.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.bt_batdau.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.bt_batdau.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.bt_batdau.FillColor = System.Drawing.Color.Black;
+            this.bt_batdau.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_batdau.ForeColor = System.Drawing.Color.White;
+            this.bt_batdau.Location = new System.Drawing.Point(587, 106);
+            this.bt_batdau.Name = "bt_batdau";
+            this.bt_batdau.Size = new System.Drawing.Size(183, 48);
+            this.bt_batdau.TabIndex = 6;
+            this.bt_batdau.Text = "Bắt đầu";
+            // 
+            // panel_cm
+            // 
+            this.panel_cm.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel_cm.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel_cm.BorderThickness = 5;
+            this.panel_cm.Controls.Add(this.bt_batdau);
+            this.panel_cm.Location = new System.Drawing.Point(171, 422);
+            this.panel_cm.Name = "panel_cm";
+            this.panel_cm.Size = new System.Drawing.Size(789, 166);
+            this.panel_cm.TabIndex = 7;
+            // 
+            // Giaithuong_DN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.panel_cm);
+            this.Controls.Add(this.cua_2);
+            this.Controls.Add(this.cua_1);
+            this.Controls.Add(this.btn_tieptuc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2PictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Giaithuong_DN";
+            this.Text = "Giaithuong_DN";
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cua_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cua_2)).EndInit();
+            this.panel_cm.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button btn_tieptuc;
+        private System.Windows.Forms.PictureBox cua_1;
+        private System.Windows.Forms.PictureBox cua_2;
+        private Guna.UI2.WinForms.Guna2Button bt_batdau;
+        private Guna.UI2.WinForms.Guna2Panel panel_cm;
+    }
+}
