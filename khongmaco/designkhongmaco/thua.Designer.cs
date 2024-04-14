1b41398fdc5cfb9ed08251c2680517fdda20ff5c
as123loinguyen <163889000+as123loinguyen@users.noreply.github.com>
2024-04-14 16:04:15 +0700

    Add files via upload

---
+++ khongmaco/designkhongmaco/thua.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace designkhongmaco
+{
+    partial class thua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.soluongthua = new System.Windows.Forms.TextBox();
+            this.doanlai = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderColor = System.Drawing.Color.Red;
+            this.guna2Panel1.BorderRadius = 10;
+            this.guna2Panel1.BorderThickness = 4;
+            this.guna2Panel1.Controls.Add(this.label3);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.soluongthua);
+            this.guna2Panel1.Controls.Add(this.doanlai);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Location = new System.Drawing.Point(69, 21);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(453, 247);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // soluongthua
+            // 
+            this.soluongthua.Location = new System.Drawing.Point(162, 95);
+            this.soluongthua.Name = "soluongthua";
+            this.soluongthua.Size = new System.Drawing.Size(29, 20);
+            this.soluongthua.TabIndex = 2;
+            // 
+            // doanlai
+            // 
+            this.doanlai.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.doanlai.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doanlai.Location = new System.Drawing.Point(329, 184);
+            this.doanlai.Name = "doanlai";
+            this.doanlai.Size = new System.Drawing.Size(81, 39);
+            this.doanlai.TabIndex = 1;
+            this.doanlai.Text = "Đoán lại";
+            this.doanlai.UseVisualStyleBackColor = false;
+            this.doanlai.Click += doanlai_Click;
+
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BẠN ĐÃ KHÔNG VƯỢT QUA VÒNG NÀY!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bạn chỉ đoán được";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(197, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "sản phẩm";
+            // 
+            // thua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::designkhongmaco.Properties.Resources.hhh;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(602, 334);
+            this.Controls.Add(this.guna2Panel1);
+            this.MaximumSize = new System.Drawing.Size(680, 420);
+            this.Name = "thua";
+            this.Text = "thua";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button doanlai;
+        private System.Windows.Forms.TextBox soluongthua;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
